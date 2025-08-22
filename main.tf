8db541bb2e48f1dc6644101850dfd23af0a3fb29
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2025-08-22 16:53:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "google_storage_bucket-019" {
-  name          = "infra-cicd-bucket-jenkins-041"
+resource "google_storage_bucket" "google_storage_bucket-919" {
+  name          = "infra-cicd-bucket-jenkins-081"
   location      = "US"
   project       = "hcl2025"
   force_destroy = true
